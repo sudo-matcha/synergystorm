e3654e50a832470a91e98228332e1fdb52d768ef
sudo-matcha <jskresl@gmail.com>
2024-04-23 10:35:30 -0400

    new file:   README 	modified:   install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 sudo cp ./dist/synergystorm/synergystorm /usr/bin
-sudo rsync -a ./dist/synergystorm/_internal/ /usr/bin/_internal/
+# sudo rsync -a ./dist/synergystorm/_internal/ /usr/bin/_internal/
 # sudo mkdir /usr/bin/_internal/seleniumwire
 # sudo python3 -m seleniumwire extractcert /usr/bin/_internal/seleniumwire/ca.crt 
